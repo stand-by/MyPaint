142118817eb93f81a61ec9b638a270f8cc03d68c
eugene <eugene.rublenko@gmail.com>
2016-08-13 12:22:14 +0300

    synchronize scene's and view's coordinate systems

--- director.cpp
+++ director.cpp
@@ -3,6 +3,12 @@
 Director::Director(QObject *parent): QObject(parent) {
     scene = new QGraphicsScene(this);
     view = new View;
+
+    //synchronize scene's and view's coordinate systems
+    view->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    scene->setSceneRect(-1.25, -1.25, 2.5, 2.5);
+
+
 }
 
 Director::~Director() {
