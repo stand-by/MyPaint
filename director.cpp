0cecb12664bab1b22514dbfcbc76e18fa94854c3
eugene <eugene.rublenko@gmail.com>
2016-08-13 13:46:57 +0300

    start using pencil instance in Director constructor

--- director.cpp
+++ director.cpp
@@ -1,14 +1,18 @@
 #include "director.h"
 
 Director::Director(QObject *parent): QObject(parent) {
-    scene = new QGraphicsScene(this);
     view = new View;
+    scene = new QGraphicsScene(this);
+    pencil = new Pencil(this);
 
     //synchronize scene's and view's coordinate systems
     view->setAlignment(Qt::AlignLeft | Qt::AlignTop);
     scene->setSceneRect(-1.25, -1.25, 2.5, 2.5);
 
     view->setScene(scene);
+
+    pencil->bindSceneAndView(scene, view);
+    //???scene->addItem(pencil->getGraphicsItem());???
 }
 
 Director::~Director() {
