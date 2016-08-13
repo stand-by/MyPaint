7f9ddb63cb86d28a328fe39144150dc7e0629cf3
eugene <eugene.rublenko@gmail.com>
2016-08-13 19:44:04 +0300

    make constructor for AbstractDrawingTool class

--- abstractdrawingtool.cpp
+++ abstractdrawingtool.cpp
@@ -1,7 +1,9 @@
 #include "abstractdrawingtool.h"
 
 AbstractDrawingTool::AbstractDrawingTool(QObject *parent) : QObject(parent) {
-
+    canDraw = false;
+    pen = QPen(Qt::black);
+    pen.setWidth(1);
 }
 AbstractDrawingTool::~AbstractDrawingTool() {
 
