3828f43b6cfe6a78b2bdcbd538f38f7320d9f1df
eugene <eugene.rublenko@gmail.com>
2016-08-13 20:14:19 +0300

    add definitions of slots in PencilDrawingTool class

--- pencildrawingtool.cpp
+++ pencildrawingtool.cpp
@@ -1,2 +1,19 @@
 #include "pencildrawingtool.h"
 
+void PencilDrawingTool::mousePress_reciever(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        canDraw = true;
+        scene->addLine(event->x(),event->y(),event->x(),event->y(),pen);
+        oldPos = event->pos();
+    }
+}
+void PencilDrawingTool::mouseMove_reciever(QMouseEvent *event) {
+    if (canDraw){
+        newPos = event->pos();
+        scene->addLine(QLine(oldPos, newPos), pen);
+        oldPos = newPos;
+     }
+}
+void PencilDrawingTool::mouseRelease_reciever(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) canDraw = false;
+}
