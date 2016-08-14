6271c0aaffc15c81bda18f9442fa9a742f6ce8af
eugene <eugene.rublenko@gmail.com>
2016-08-14 11:51:20 +0300

    change size of tool icons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,27 +19,27 @@ void MainWindow::on_actionCursor_triggered() {
     director->setCursorTool();
 }
 void MainWindow::on_actionPencil_triggered() {
-    this->setCursor(QCursor(QPixmap(":/icons/Pencil-48.png")));
+    this->setCursor(QCursor(QPixmap(":/icons/Pencil-24.png")));
     director->setPencilTool();
 }
 void MainWindow::on_actionLine_triggered() {
-    this->setCursor(QCursor(QPixmap(":/icons/Line-48.png")));
+    this->setCursor(QCursor(QPixmap(":/icons/Line-24.png")));
     director->setLineTool();
 }
 void MainWindow::on_actionRectangle_triggered() {
-    this->setCursor(QCursor(QPixmap(":/icons/Rectangle\ Stroked-48.png")));
+    this->setCursor(QCursor(QPixmap(":/icons/Rectangle\ Stroked-24.png")));
     director->setRectangleTool();
 }
 void MainWindow::on_actionFilled_Rectangle_triggered() {
-    this->setCursor(QCursor(QPixmap(":/icons/Rectangle-48.png")));
+    this->setCursor(QCursor(QPixmap(":/icons/Rectangle-24.png")));
     director->setFilledRectangleTool();
 }
 void MainWindow::on_actionEllipse_triggered() {
-    this->setCursor(QCursor(QPixmap(":/icons/Ellipse\ Stroked-48.png")));
+    this->setCursor(QCursor(QPixmap(":/icons/Ellipse\ Stroked-24.png")));
     director->setEllipseTool();
 }
 void MainWindow::on_actionFilled_Ellipse_triggered() {
-    this->setCursor(QCursor(QPixmap(":/icons/Ellipse-48.png")));
+    this->setCursor(QCursor(QPixmap(":/icons/Ellipse-24.png")));
     director->setFilledEllipseTool();
 }
 void MainWindow::on_actionColor_Picker_triggered() {
@@ -48,4 +48,3 @@ void MainWindow::on_actionColor_Picker_triggered() {
 void MainWindow::on_actionEraser_triggered() {
 
 }
-
