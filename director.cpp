bcbdcc4f2a17effcc7a1bbf43a4e3cd5428a4c5c
eugene <eugene.rublenko@gmail.com>
2016-08-14 10:18:28 +0300

    implement setPencilTool and setLineTool methods in Director class

--- director.cpp
+++ director.cpp
@@ -1,17 +1,18 @@
 #include "director.h"
 
 Director::Director(QObject *parent): QObject(parent) {
+    tool = NULL;
     view = new View;
     scene = new QGraphicsScene(this);
-    tool = new FilledEllipseDrawingTool(this);
 
     //synchronize scene's and view's coordinate systems
     view->setAlignment(Qt::AlignLeft | Qt::AlignTop);
     scene->setSceneRect(-1.25, -1.25, 2.5, 2.5);
 
     view->setScene(scene);
+    this->setPencilTool();
 
-    tool->bindSceneAndView(scene, view);
+    //tool->bindSceneAndView(scene, view);
     //rectangle->bindSceneAndView(scene, view);
     //line->bindSceneAndView(scene, view);
     //pencil->bindSceneAndView(scene, view);
@@ -28,10 +29,14 @@ void Director::setCursorTool() {
 
 }
 void Director::setPencilTool() {
-
+    if(tool != NULL) delete tool;
+    tool = new PencilDrawingTool(this);
+    tool->bindSceneAndView(scene, view);
 }
 void Director::setLineTool() {
-
+    if(tool != NULL) delete tool;
+    tool = new LineDrawingTool(this);
+    tool->bindSceneAndView(scene, view);
 }
 void Director::setRectangleTool() {
 
