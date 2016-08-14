2813404da7b1e33ecf0d7a83ee8386e0cd23b742
eugene <eugene.rublenko@gmail.com>
2016-08-14 10:28:25 +0300

    implement setRectangleTool and setFilledRectangleTool methods in Director class

--- director.cpp
+++ director.cpp
@@ -10,7 +10,7 @@ Director::Director(QObject *parent): QObject(parent) {
     scene->setSceneRect(-1.25, -1.25, 2.5, 2.5);
 
     view->setScene(scene);
-    this->setPencilTool();
+    this->setCursorTool();
 
     //tool->bindSceneAndView(scene, view);
     //rectangle->bindSceneAndView(scene, view);
@@ -26,7 +26,7 @@ Director::~Director() {
 }
 
 void Director::setCursorTool() {
-
+    if(tool != NULL) delete tool;
 }
 void Director::setPencilTool() {
     if(tool != NULL) delete tool;
@@ -39,10 +39,14 @@ void Director::setLineTool() {
     tool->bindSceneAndView(scene, view);
 }
 void Director::setRectangleTool() {
-
+    if(tool != NULL) delete tool;
+    tool = new RectangleDrawingTool(this);
+    tool->bindSceneAndView(scene, view);
 }
 void Director::setFilledRectangleTool() {
-
+    if(tool != NULL) delete tool;
+    tool = new FilledRectangleDrawingTool(this);
+    tool->bindSceneAndView(scene, view);
 }
 void Director::setEllipseTool() {
 
